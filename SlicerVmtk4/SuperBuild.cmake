9aa92ae33a4035582a79aff9ee29cfb32e6067c4
Daniel Haehn <dh@randi2.de>
2011-04-27 10:33:37 -0400

    Adding new Slicer4 Extension infrastructure.

---
+++ SlicerVmtk4/SuperBuild.cmake
@@ -0,0 +1,64 @@
+
+#-----------------------------------------------------------------------------
+# Git protocole option
+#-----------------------------------------------------------------------------
+option(Slicer_USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+
+set(git_protocol "git")
+if(NOT Slicer_USE_GIT_PROTOCOL)
+  set(git_protocol "http")
+endif()
+
+#-----------------------------------------------------------------------------
+# Enable and setup External project global properties
+#-----------------------------------------------------------------------------
+INCLUDE(ExternalProject)
+
+set(ep_base        "${CMAKE_BINARY_DIR}")
+SET(ep_common_args
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  #-DBUILD_TESTING:BOOL=OFF
+  )
+  
+  
+# Compute -G arg for configuring external projects with the same CMake generator:
+if(CMAKE_EXTRA_GENERATOR)
+  set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
+else()
+  set(gen "${CMAKE_GENERATOR}")
+endif()
+
+#-----------------------------------------------------------------------------
+# SlicerVmtk4 dependencies
+#-----------------------------------------------------------------------------
+set(EMS_DEPENDENCIES)
+include(SuperBuild/VMTK.cmake)
+
+#-----------------------------------------------------------------------------
+# Configure and build VMTK
+#------------------------------------------------------------------------------
+
+set(SlicerVmtk4_DEPENDENCIES VMTK)
+
+set(proj SlicerVmtk4)
+ExternalProject_Add(${proj}
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+  BINARY_DIR ${proj}-build
+  CMAKE_GENERATOR ${gen}
+  CMAKE_ARGS
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
+    -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}
+    -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE}
+    -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+    -D${proj}_SUPERBUILD:BOOL=OFF
+    #-DCTEST_CONFIGURATION_TYPE:STRING=${CTEST_CONFIGURATION_TYPE}
+    # Slicer
+    -DSlicer_DIR:PATH=${Slicer_BINARY_DIR}
+    # VMTK
+    -DVMTK_DIR:PATH=${VMTK_DIR}
+  DEPENDS 
+    ${SlicerVmtk4_DEPENDENCIES}
+  )
+  
---
+++ SlicerVmtk4/SuperBuild/VMTK.cmake
@@ -0,0 +1,66 @@
+
+# Fetch the VMTK Package
+#-----------------------------------------------------------------------------
+
+# Sanity checks
+if(DEFINED VMTK_DIR AND NOT EXISTS ${VMTK_DIR})
+  message(FATAL_ERROR "VMTK_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+set(proj VMTK)
+
+# re-define git protocol if necessary
+if(NOT DEFINED git_protocol)
+  set( git_protocol "git")
+endif()
+
+if(NOT DEFINED VMTK_DIR)
+  message(STATUS "Adding external project: ${proj}")
+  ExternalProject_Add(${proj}
+    GIT_REPOSITORY "${git_protocol}://github.com/haehn/vmtk.git"
+    GIT_TAG "origin/superbuild"
+    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}
+    BINARY_DIR ${proj}-build
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      ${ep_common_args}
+      -DBUILDNAME:STRING=${BUILDNAME}
+      -DSITE:STRING=${SITE}
+      -DMAKECOMMAND:STRING=${MAKECOMMAND}
+      -DCMAKE_BUILD_TYPE:STRING=${build_type}
+      -DBUILD_SHARED_LIBS:BOOL=ON
+      -DBUILD_EXAMPLES:BOOL=OFF
+      -DBUILD_TESTING:BOOL=OFF
+      -DBUILD_DOCUMENTATION:BOOL=OFF
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/Install
+      -DVTK_VMTK_WRAP_PYTHON:BOOL=ON
+      # we don't want superbuild since it will override our CMake settings
+      -DVMTK_USE_SUPERBUILD:BOOL=OFF
+      -DVMTK_CONTRIB_SCRIPTS:BOOL=ON
+      -DVMTK_VTK_CONTRIB:BOOL=ON
+      -DVMTK_MINIMAL_INSTALL:BOOL=OFF
+      -DVMTK_ENABLE_DISTRIBUTION:BOOL=OFF
+      -DVMTK_WITH_LIBRARY_VERSION:BOOL=OFF
+      -DVTK_VMTK_IN_SLICER4:BOOL=ON
+      # we do not want tetgen..
+      -DVMTK_BUILD_TETGEN:BOOL=OFF
+      # we want the vmtk scripts :)
+      -DVMTK_SCRIPTS_ENABLED:BOOL=ON
+      # we do not want cocoa, go away :)
+      -DVTK_VMTK_USE_COCOA:BOOL=OFF
+      # we use Slicer's VTK and ITK
+      -DUSE_SYSTEM_VTK:BOOL=ON
+      -DUSE_SYSTEM_ITK:BOOL=ON
+      -DITK_DIR:PATH=${ITK_DIR}
+      -DVTK_DIR:PATH=${VTK_DIR}
+      -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+    INSTALL_COMMAND ""
+    DEPENDS 
+      ${VMTK_DEPENDENCIES}
+    )
+  set(VMTK_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}-build)
+endif()
+# End of VMTK External
+#-----------------------------------------------------------------------------
