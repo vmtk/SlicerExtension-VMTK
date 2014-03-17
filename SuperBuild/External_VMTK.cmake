bd031746a645744f150de343f128203ce747ee57
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-03-17 17:47:46 -0400

    Simplify VMTK Slicer extension build system
    Rename extension into "SlicerExtension-VMTK"
    
    Update build system based on current Slicer Superbuild template
    
    Remove unneeded "LibraryModule" loadable module. Packaging the VMTK
    python modules should be sufficient.
    
    Add VMTK to CPACK_INSTALL_CMAKE_PROJECTS so that associated project
    is installed.
    
    Use "slicerMacroBuildScriptedModule" instead of the deprecated "ctkMacroCompilePythonScript"
    
    Within VMTK external project:
    
    * Checkout more recent version of VMTK
    
    * Set install sub directory using Slicer_INSTALL_* variables

--- SuperBuild/External_VMTK.cmake
+++ SuperBuild/External_VMTK.cmake
@@ -1,70 +1,54 @@
-# Fetch the VMTK Package
-#-----------------------------------------------------------------------------
-
-# Make sure this file is included only once
-get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
-if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
-  return()
-endif()
-set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
-
-# Sanity checks
-if(DEFINED VMTK_DIR AND NOT EXISTS ${VMTK_DIR})
-  message(FATAL_ERROR "VMTK_DIR variable is defined but corresponds to non-existing directory")
-endif()
-
-# Set dependency list
-set(VMTK_DEPENDENCIES "")
-
-SlicerMacroCheckExternalProjectDependency(VMTK)
 set(proj VMTK)
 
-if(NOT DEFINED ${proj}_DIR)
-  #message(STATUS "Adding external project: ${proj}")
-  
-  # Set CMake OSX variable to pass down the external project
-  set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
-  if(APPLE)
-    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
-      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
-      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
-  endif()  
+# Set dependency list
+set(${proj}_DEPENDS "")
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj)
+
+if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+  message(FATAL_ERROR "Enabling ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj} is not supported !")
+endif()
+
+# Sanity checks
+if(DEFINED Foo_DIR AND NOT EXISTS ${Foo_DIR})
+  message(FATAL_ERROR "Foo_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   if(NOT DEFINED git_protocol)
     set(git_protocol "git")
-  endif()  
-  
+  endif()
+
   ExternalProject_Add(${proj}
-    GIT_REPOSITORY "${git_protocol}://github.com/lantiga/vmtk.git"
-    GIT_TAG "origin/itk4"
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}
+    ${${proj}_EP_ARGS}
+    GIT_REPOSITORY "${git_protocol}://github.com/VMTK/vmtk.git"
+    GIT_TAG "e3924f9d3d8ddd419ed6683f3e7a3fdacb6069db"
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
-    CMAKE_GENERATOR ${gen}
-    CMAKE_ARGS
-      ${ep_common_args}
-      -DCMAKE_BUILD_TYPE:STRING=${build_type}
-      -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
-      -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
-      -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
+    CMAKE_CACHE_ARGS
+      #-DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
+      #-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
+      #-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build/${Slicer_QTLOADABLEMODULES_LIB_DIR}
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_DOCUMENTATION:BOOL=OFF
       -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/SlicerVmtk-build
       # installation location for the pypes/scripts
-      -DVMTK_INSTALL_BIN_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DVMTK_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DVMTK_SCRIPTS_INSTALL_BIN_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DVMTK_SCRIPTS_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DPYPES_INSTALL_BIN_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DPYPES_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DVMTK_CONTRIB_SCRIPTS_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
-      -DVMTK_CONTRIB_SCRIPTS_INSTALL_BIN_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}/pypes
+      -DVMTK_INSTALL_BIN_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DVMTK_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DVMTK_SCRIPTS_INSTALL_BIN_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DVMTK_SCRIPTS_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DPYPES_INSTALL_BIN_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DPYPES_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DVMTK_CONTRIB_SCRIPTS_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
+      -DVMTK_CONTRIB_SCRIPTS_INSTALL_BIN_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}/pypes
       # installation location for all vtkvmtk stuff
-      -DVTK_VMTK_INSTALL_BIN_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}
-      -DVTK_VMTK_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}
-      -DVTK_VMTK_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_QTLOADABLEMODULES_LIB_DIR}
+      -DVTK_VMTK_INSTALL_BIN_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_BIN_DIR}
+      -DVTK_VMTK_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_LIB_DIR}
+      -DVTK_VMTK_MODULE_INSTALL_LIB_DIR:PATH=${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}
       -DVTK_VMTK_WRAP_PYTHON:BOOL=ON
       # we don't want superbuild since it will override our CMake settings
       -DVMTK_USE_SUPERBUILD:BOOL=OFF
@@ -82,15 +66,13 @@ if(NOT DEFINED ${proj}_DIR)
       -DITK_DIR:PATH=${ITK_DIR}
       -DVTK_DIR:PATH=${VTK_DIR}
     INSTALL_COMMAND ""
-    DEPENDS 
-      ${VMTK_DEPENDENCIES}
+    DEPENDS
+      ${${proj}_DEPENDS}
     )
   set(${proj}_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
 
 else()
-  # The project is provided using <proj>_DIR, nevertheless since other project may depend on <proj>,
-  # let's add an 'empty' one
-  SlicerMacroEmptyExternalProject(${proj} "${${proj}_DEPENDENCIES}")
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDS})
 endif()
-# End of VMTK External
-#-----------------------------------------------------------------------------
+
+mark_as_superbuild(${proj}_DIR:PATH)
