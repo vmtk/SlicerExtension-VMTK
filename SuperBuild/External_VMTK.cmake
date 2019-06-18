42f988edc1e7835a270301bea088db1c6091a97b
Andras Lasso <lasso@cs.queensu.ca>
2019-06-18 11:52:40 -0400

    Attempt to fix Mac build error
    Merged changes from other VMTK repositories that might fix the build error on Mac

--- SuperBuild/External_VMTK.cmake
+++ SuperBuild/External_VMTK.cmake
@@ -25,7 +25,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "91bc715e79e55ac63e83ffcefce66e67b52fac69"
+    "01855002f11db665f4d8ca1528cf9e95d111d96b"
     QUIET
     )
 
