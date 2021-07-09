vmtk/SlicerExtension-VMTK

commit be5ac3a2c47c75b1a96b5b395a4364c119b7a2e2

COMP: Fix build against VTK8 specifying VMTK_USE_VTK9 build option

SuperBuild/External_VMTK.cmake
@@ -20,6 +20,12 @@ endif()
 
 if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
+  set(VMTK_USE_VTK9 ON)
+  if(Slicer_VTK_VERSION_MAJOR VERSION_LESS "9")
+    set(VMTK_USE_VTK9 OFF)
+  endif()
+  ExternalProject_Message(${proj} "VMTK_USE_VTK9:${VMTK_USE_VTK9}")
+
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
   if(VTK_WRAP_PYTHON)
     list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS
@@ -100,6 +106,8 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       # macOS
       -DCMAKE_INSTALL_NAME_TOOL:FILEPATH=${CMAKE_INSTALL_NAME_TOOL}
       -DCMAKE_MACOSX_RPATH:BOOL=0
+      # Options
+      -DVMTK_USE_VTK9:BOOL=${VMTK_USE_VTK9}
       ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}
     INSTALL_COMMAND ""
     DEPENDS

commit 389c5d294b085d9ae97dbe0d0aa4a145655dc6a8

COMP: Update VMTK to fix Visual Studio build
This commit updates VMTK to fix a regression introduced in be5ac3a (COMP: Fix
build against VTK8 specifying VMTK_USE_VTK9 build option)

List of changes:

$ git shortlog bdddd70..671b54d --no-merges
Jean-Christophe Fillion-Robin (2):
      COMP: Update pragma use in vtkvmtkSimplifyVoronoiDiagram to Fix Visual Studio build
      COMP: Fix Visual Studio build of vtkvmtkRBFInterpolation when VTK_LEGACY_REMOVE is ON

SuperBuild/External_VMTK.cmake
@@ -50,7 +50,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "bdddd70cc2ed40a10ba85bff1279d910db31fc1e"
+    "671b54d7b3b243d3d2ce662ffa7f678f53ee18a1"
     QUIET
     )
 