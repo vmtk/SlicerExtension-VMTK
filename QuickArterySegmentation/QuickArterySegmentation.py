32435f3864280e693ca43e7dd9126d030d2f0ccf
Saleem Edah-Tally <set@chirvasc-60.pro>
2025-10-24 17:34:42 +0200

    Improve setting the default values of a parameter node.
    QuickArterySegmentation
    
    Add each parameter with its default value if it does not exist in the parameter
    node.
    
    The previous 'INITIALIZED' parameter is dropped, so that new parameters that are
    not present in saved scenes can be handled.

--- QuickArterySegmentation/QuickArterySegmentation.py
+++ QuickArterySegmentation/QuickArterySegmentation.py
@@ -236,15 +236,18 @@ class QuickArterySegmentationWidget(ScriptedLoadableModuleWidget, VTKObservation
     if not self._parameterNode:
       return
 
-    if self._parameterNode.HasParameter(ROLE_INITIALIZED):
-      return
-
-    self._parameterNode.SetParameter(ROLE_INPUT_INTENSITY_TOLERANCE, str(100))
-    self._parameterNode.SetParameter(ROLE_INPUT_NEIGHBOURHOOD_SIZE, str(2.0))
-    self._parameterNode.SetParameter(ROLE_OPTION_SMOOTHING, str(0))
-    self._parameterNode.SetParameter(ROLE_INPUT_KERNEL_SIZE, str(1.1))
-    self._parameterNode.SetParameter(ROLE_OPTION_EXTRACT_CENTERLINES, str(0))
-    self._parameterNode.SetParameter(ROLE_INITIALIZED, str(1))
+    # Ensure all parameters exist in the parameter node.
+    # Existing parameters are not modified.
+    if (not self._parameterNode.HasParameter(ROLE_INPUT_INTENSITY_TOLERANCE)):
+      self._parameterNode.SetParameter(ROLE_INPUT_INTENSITY_TOLERANCE, str(100))
+    if (not self._parameterNode.HasParameter(ROLE_INPUT_NEIGHBOURHOOD_SIZE)):
+      self._parameterNode.SetParameter(ROLE_INPUT_NEIGHBOURHOOD_SIZE, str(2.0))
+    if (not self._parameterNode.HasParameter(ROLE_OPTION_SMOOTHING)):
+      self._parameterNode.SetParameter(ROLE_OPTION_SMOOTHING, str(0))
+    if (not self._parameterNode.HasParameter(ROLE_INPUT_KERNEL_SIZE)):
+      self._parameterNode.SetParameter(ROLE_INPUT_KERNEL_SIZE, str(1.1))
+    if (not self._parameterNode.HasParameter(ROLE_OPTION_EXTRACT_CENTERLINES)):
+      self._parameterNode.SetParameter(ROLE_OPTION_EXTRACT_CENTERLINES, str(0))
 
   def onApplyButton(self) -> None:
     """
@@ -643,4 +646,3 @@ ROLE_INPUT_KERNEL_SIZE = "InputKernelSize"
 ROLE_OPTION_EXTRACT_CENTERLINES = "OptionExtractCenterlines"
 ROLE_OUTPUT_CENTERLINE_MODEL = "OutputCenterlineModel" # Set in logic
 ROLE_OUTPUT_CENTERLINE_CURVE = "OutputCenterlineCurve" # Set in logic
-ROLE_INITIALIZED = "Initialized"
